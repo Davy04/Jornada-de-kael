afa3e814f576fd62320bc4c04214e79fd8383aef
Davy <davywoolley@gmail.com>
2024-04-01 12:43:24 -0300

    começando a movimentação

---
+++ Jornada de Kael/Assets/Scripts/Character.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Character : MonoBehaviour
+{
+    [Header("Controls")]
+    public float playerSpeed = 5.0f;
+    public float sprintSpeed = 7.0f;
+    public float jumpHeight = 0.8f;
+    public float gravityMultiplier = 2.0f;
+    public float rotationSpeed = 5.0f;
+
+    [Header("Animation Smoothing")]
+    [Range(0f, 1f)]
+    public float speedDampTime = 0.1f;
+    [Range(0f, 1f)]
+    public float velocityDampTime = 0.9f;
+    [Range(0f, 1f)]
+    public float rotationDampTime = 0.2f;
+    [Range(0f, 1f)]
+    public float airControl = 0.1f;
+
+    public StateMachine movementSM;
+    public StandingState standing;
+    public JumpingState jumping;
+    public LandingState landing;
+    public SprintState sprinting;
+
+    [HideInInspector]
+    public float gravityValue = -9.81f;
+    [HideInInspector]
+    public float normalColliderHeight;
+    [HideInInspector]
+    public CharacterController controller;
+    [HideInInspector]
+    public PlayerInput playerInput;
+    [HideInInspector]
+    public Animator animator;
+    [HideInInspector]
+    public Vector3 playerVelocity;
+    [HideInInspector]
+    public Transform cameraTransform;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        playerInput = GetComponent<PlayerInput>();
+        cameraTransform = Camera.main.transform;
+
+        movementSM = new StateMachine();
+        standing = new StandingState(this,movementSM);
+        jumping = new JumpingState(this, movementSM);
+        landing = new LandingState(this, movementSM);
+        sprinting = new SprintState(this, movementSM);
+
+        movementSM.Initialize(standing);
+
+        normalColliderHeight = controller.height;
+        gravityValue *= gravityMultiplier;
+    }
+
+    private void Update()
+    {
+        movementSM.currentState.HandleInput();
+        
+        movementSM.currentState.LogicUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        movementSM.currentState.PhysicsUpdate();
+    }
+}
